afbd56bbf770d7fafb5c96d0894d9f30c0adac6c
Томко Владислав Викторович <tomko@dev.vtb.ru>
2023-03-14 17:18:27 +0300

    add unit test

--- src/test/java/ru/neoflex/conveor/service/CalculateServiceTest.java
+++ src/test/java/ru/neoflex/conveor/service/CalculateServiceTest.java
@@ -14,12 +14,29 @@ public class CalculateServiceTest {
     private final static BigDecimal TOTAL_AMOUNT_REQUEST = new BigDecimal("15000.00");
     private final static BigDecimal TOTAL_AMOUNT_NOT_INSURANCE = new BigDecimal("15000.00");
     private final static BigDecimal TOTAL_AMOUNT_IS_INSURANCE = new BigDecimal("30000.00");
+    private final static BigDecimal RATE_IS_INSURANCE_IS_SALARY_CLIENT = new BigDecimal("8.00");
+    private final static BigDecimal RATE_IS_INSURANCE_NOT_SALARY_CLIENT = new BigDecimal("10.00");
+    private final static BigDecimal RATE_NOT_INSURANCE_IS_SALARY_CLIENT = new BigDecimal("11.00");
+    private final static BigDecimal RATE_NOT_INSURANCE_NOT_SALARY_CLIENT = new BigDecimal("13.00");
+
 
     @Test
-    public void calculateTotalAmountTest() {
+    public void calculateTotalAmount() {
         Assertions.assertThat(calculatorService.calculateTotalAmount(TOTAL_AMOUNT_REQUEST, false))
                 .isEqualTo(TOTAL_AMOUNT_NOT_INSURANCE);
         Assertions.assertThat(calculatorService.calculateTotalAmount(TOTAL_AMOUNT_REQUEST, true))
                 .isEqualTo(TOTAL_AMOUNT_IS_INSURANCE);
+    }
+
+    @Test
+    public void calculateRate() {
+        Assertions.assertThat(calculatorService.calculateRate(true, true))
+                .isEqualTo(RATE_IS_INSURANCE_IS_SALARY_CLIENT);
+        Assertions.assertThat(calculatorService.calculateRate(true, false))
+                .isEqualTo(RATE_IS_INSURANCE_NOT_SALARY_CLIENT);
+        Assertions.assertThat(calculatorService.calculateRate(false, true))
+                .isEqualTo(RATE_NOT_INSURANCE_IS_SALARY_CLIENT);
+        Assertions.assertThat(calculatorService.calculateRate(false, false))
+                .isEqualTo(RATE_NOT_INSURANCE_NOT_SALARY_CLIENT);
     }
 }
